7e689c1e3754e1b93f6d406fd1b377c2133db105
Rodrigo Guevara Civit <rgcivitt@gmail.com>
2026-08-27 13:27:43 -0300

    capacitor

---
+++ android/app/src/main/java/com/esthetikare/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.esthetikare.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.esthetikare.app',
+  appName: 'esthetikare',
+  webDir: 'dist'
+};
+
+export default config;
